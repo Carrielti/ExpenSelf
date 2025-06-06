60f7ff1fe7ca7c54da2e473572e3771334cfa99b
Carrielti <lucasgccardoso1@gmail.com>
2025-06-05 21:21:07 -0300

    Atualiza app.py para uso da variável FIREBASE_KEY

--- firebase-api-expenself/app.py
+++ firebase-api-expenself/app.py
@@ -34,4 +34,7 @@ def enviar():
     return jsonify({"mensagem": "Despesa enviada com sucesso!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
